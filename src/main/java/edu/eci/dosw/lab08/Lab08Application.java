d9ec5d14d3475286fd0b27cece53607ef4d9e557
Diego Montes <diego.montes-b@mail.escuelaing.edu.co>
2026-03-11 17:26:21 -0500

    project-structure

---
+++ src/main/java/edu/eci/dosw/lab08/Lab08Application.java
@@ -0,0 +1,17 @@
+package edu.eci.dosw.lab08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal de la aplicación Spring Boot.
+ * Lab 08 - Kevyn, Diego, Juliana, Juan
+ */
+@SpringBootApplication
+public class Lab08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab08Application.class, args);
+    }
+
+}
